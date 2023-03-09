860eb35489167f434fbc0800cefce1c339ed07d3
roma <palchyk.roman@gmail.com>
2023-03-09 10:42:35 +0200

    New commit

--- services/db.service.js
+++ services/db.service.js
@@ -13,6 +13,8 @@ class DbService {
         });
     }
 
+
+
     async isDataExists(data, collection) {
         const connection = await mongoDB.connect(url);
         const db = connection.db('CryptoProject');
